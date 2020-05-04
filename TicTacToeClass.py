27b05cd1d81a48fdf4e888b656cb370f38805c81
Alkantou <alexkantounis@gmail.com>
2020-05-05 00:42:49 +0300

    Tic Tac Toe Excercise

---
+++ TicTacToeClass.py
@@ -0,0 +1,38 @@
+class TicTacToeClass:
+
+    def __init__(self):
+        self.cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.current_player = 'X'
+        self.moves = 0
+
+    def next_move(self,row_index:int,column_index:int) -> None:
+        #self.cells[0][0] = 'X'
+        self.cells[row_index][column_index]= self.current_player
+        self.moves = self.moves + 1
+
+    def switch_player(self):
+        if self.current_player == 'X':
+            self.current_player = 'O'
+        else:
+            self.current_player = 'X'
+
+    def game_over(self):
+        return self.tictactoe() or self.nomovesleft()
+
+    def nomovesleft(self):
+        return self.moves == 9
+
+    def tictactoe(self):
+        column_index = 0
+        for row_index in range(3):
+             if self.cells[row_indexw_index][column_index] == self.cells[row_index][column_index + 1] and self.cells[row_index][column_index + 1] == self.cells[row_index][column_index + 2]:
+                 return True
+        row_index = 0
+
+
+
+
+
+
+
+
