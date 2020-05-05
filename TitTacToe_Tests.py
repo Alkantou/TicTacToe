ba3e508aaa5c0a33777d5c4a44bd41d7b7826c77
Karolos <karolos.kantounis@gmail.com>
2020-05-05 16:49:35 -0400

    Added some unit tests
    The ones that are green you do not need to change
    
    Try to make the red ones become green
    
    Try to implement the last one according to the instructions in the comments
    
    I also as an example in the learning example of how to raise an error

---
+++ TitTacToe_Tests.py
@@ -0,0 +1,77 @@
+import pytest
+
+
+from TicTacToeClass import TicTacToeClass
+
+
+def test_first_player_is_X():
+    assert TicTacToeClass().current_player == "X"
+
+
+def test_switch_player():
+     game = TicTacToeClass()
+     game.switch_player()
+     assert game.current_player == "O"
+     game.switch_player()
+     assert game.current_player == "X"
+
+def test_game_next_move_switches_player():
+    game = TicTacToeClass()
+    game.next_move(0,0)
+    assert game.current_player == "O"
+
+
+def test_game_invalid_index_raised_value_error():
+    game = TicTacToeClass()
+    with pytest.raises(ValueError):
+        game.next_move(-1,-1)
+
+def test_game_over_index_raised_value_error():
+    game = TicTacToeClass()
+    with pytest.raises(ValueError):
+        game.next_move(4,-1)
+
+
+def test_moves_performed():
+    game = TicTacToeClass()
+    game.next_move(0,0)
+    game.next_move(1,1)
+    assert game.moves == 2
+
+
+def test_not_possible_to_move_to_already_used_cell():
+    game = TicTacToeClass()
+    game.next_move(0,0)
+    with pytest.raises(ValueError):
+        game.next_move(0,0)
+
+
+def test_line_tic_tac():
+    game = TicTacToeClass()
+    game.next_move(0, 0)
+    game.next_move(1, 0)
+    game.next_move(0, 1)
+    game.next_move(1, 0)
+    game.next_move(0, 2)
+
+    assert game.tictactoe() == True
+
+
+def test_winner_is_X():
+    game = TicTacToeClass()
+    game.next_move(0, 0)
+    game.next_move(1, 0)
+    game.next_move(0, 1)
+    game.next_move(1, 0)
+    game.next_move(0, 2)
+
+    assert game.winner == "X"
+
+
+def test_not_possible_to_play_after_game_is_over():
+
+    # write code to play a game with a winner
+
+    # try to play a free cell should raise a value error
+
+    pass
