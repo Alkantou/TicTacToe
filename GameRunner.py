d500fc6e69543f327e86b9670173c5a16c3e767e
Alkantou <alexkantounis@gmail.com>
2020-05-14 22:10:38 +0300

    Tic Tac Toe Gamer Runner

---
+++ GameRunner.py
@@ -0,0 +1,16 @@
+# Input Player's choice of mark
+# Input Place of mark
+# Input 2nd player's mark
+
+#while game == True
+    # input place of mark
+
+from TicTacToeClass import TicTacToeClass
+# Input Player's choice of mark
+input_mark = input("Insert input mark ")
+game = TicTacToeClass(input_mark)
+while not game.game_over():
+    #print current board
+    #read user input
+    game.next_move()
+
--- TicTacToeClass.py
+++ TicTacToeClass.py
@@ -1,8 +1,8 @@
 class TicTacToeClass:
 
-    def __init__(self):
+    def __init__(self,inputmark):
         self.cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        self.current_player = 'X'
+        self.current_player = inputmark
         self.moves = 0
         self.winner = None
 
@@ -28,7 +28,12 @@ class TicTacToeClass:
              if self.cells[row_index][column_index] == self.cells[row_index][column_index + 1] and self.cells[row_index][column_index + 1] == self.cells[row_index][column_index + 2]:
                  return True
         row_index = 0
+        for column_index in range(3):
+            if self.cells[row_index][column_index] == self.cells[row_index+1][column_index] and self.cells[row_index+1][column_index] == self.cells[row_index + 2][column_index]:
+                 return True
 
+        if self.cells[0][0] == self.cells[1][1] and self.cells[1][1] == self.cells[2][2]:
+            return True
 
 
 
