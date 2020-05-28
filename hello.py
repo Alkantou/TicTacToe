ed2f97646b2043d905468935ca76bb0b53f8f93a
Alkantou <alexkantounis@gmail.com>
2020-05-28 14:32:09 +0300

    ChangedGrid

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ from gi.repository import Gtk
 
 
 class GridWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Grid Example")
+    def __init__(self, game):
+        Gtk.Window.__init__(self, title="TIC TAC TOE")
 
         grid = Gtk.Grid()
         self.add(grid)
@@ -18,13 +18,19 @@ class GridWindow(Gtk.Window):
         button4 = Gtk.Button(label="Button 4")
         button5 = Gtk.Button(label="Button 5")
         button6 = Gtk.Button(label="Button 6")
+        button7 = Gtk.Button(label="Button 7")
+        button8 = Gtk.Button(label="Button 8")
+        button9 = Gtk.Button(label="Button 9")
 
         grid.add(button1)
-        grid.attach(button2, 1, 0, 2, 1)
-        grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2)
-        grid.attach_next_to(button4, button3, Gtk.PositionType.RIGHT, 2, 1)
-        grid.attach(button5, 1, 2, 1, 1)
+        grid.attach(button2, 1, 0, 1, 1)
+        grid.attach_next_to(button3, button2, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(button4, button1, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(button5, button4, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(button6, button5, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(button7, button4, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(button8, button7, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(button9, button8, Gtk.PositionType.RIGHT, 1, 1)
 
     def on_button1_clicked(self, widget):
         #widget.set_label("Foo")
